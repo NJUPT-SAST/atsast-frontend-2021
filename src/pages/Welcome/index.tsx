dc7a6ab46a60d7827698edc922ac62076f3165a1
GCconfusing <72391674+GCconfusing@users.noreply.github.com>
2021-07-10 10:03:34 +0800

    Add files via upload

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -9,34 +9,39 @@ import moment from 'moment';
 import { now } from '@umijs/deps/compiled/lodash';
 import { string } from '@umijs/deps/compiled/yargs';
 
+
 function onPanelChange(value, mode) {
   console.log(value, mode);
 }
 
 var d = new Date();
 
-function greeting(hour) {
+function greeting(hour) {     //为给定的时间配备问候语
   var words;
-  if (hour <= 5 && hour > 3) 
+  if (hour <= 5 && hour > 3)
     words = "凌晨好，起得真早呀";
-  else if (hour >5&&hour<=10)
-    words="早上好，一起学习吧";
-  else if (hour>10&&hour<=12)
-    words="中午好，注意午休哦";
-  else if (hour>12&&hour<=18)
-    words="下午好，继续加油吧";
-  else if (hour>18&&hour<=22)
-    words="晚上好，又是收获满满的一天呢";
-    else words="深夜了，不要熬夜太晚哦";
+  else if (hour > 5 && hour <= 10)
+    words = "早上好，一起学习吧";
+  else if (hour > 10 && hour <= 12)
+    words = "中午好，注意午休哦";
+  else if (hour > 12 && hour <= 18)
+    words = "下午好，继续加油吧";
+  else if (hour > 18 && hour <= 22)
+    words = "晚上好，又是收获满满的一天呢";
+  else words = "深夜了，不要熬夜太晚哦";
   return words
 }
 
-const contentStyle = {
+const contentStyle = {/*跑马灯格式 */
   height: '300px',
   color: '#fff',
   lineHeight: '200px',  //内嵌字符height
   textAlign: 'center',
   background: '#364d79',
+};
+
+const titleStyle = {
+  textAlign: 'center',
 };
 
 export default (): React.ReactNode => {
@@ -45,17 +50,18 @@ export default (): React.ReactNode => {
     <PageContainer>
 
       <Row>
-        <Col span={5} push={19}>
-          <div className="greeting-words">
-            { greeting(d.getHours())}
-          </div>
-          <div className="site-calendar-demo-card">
-            <Calendar fullscreen={false} onPanelChange={onPanelChange} />
-          </div>,
+        <Col span={5} push={19}>{/*宽度为5，向右移动19，左侧栅格*/}
 
+          <div className="site-card-border-less-wrapper">//
+            <Card title={greeting(d.getHours())} bordered={false} >{/*右侧为卡片，标题设置为问候语 */}{/*标题格式可稍加修改*/}
+              <div className="site-calendar-demo-card">
+                <Calendar fullscreen={false} onPanelChange={onPanelChange} />{/*右侧卡片的内容部分使用日历组件  */}
+              </div>,
+            </Card>
+          </div>,
         </Col>
-        <Col span={18} pull={5}>
-          <Card hoverable bordered={false}>
+        <Col span={18} pull={5}>{/*宽度18，向左移动5，右侧栅格*/}
+          <Card hoverable bordered={false}>{/**左侧为跑马灯 */}
             <Carousel autoplay>
               <div>
                 <h3 style={contentStyle}>PicLink1</h3>
