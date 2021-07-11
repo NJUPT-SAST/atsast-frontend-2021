72ce2c51897316433c33bd8f02c01a76f934b3d3
picpo <2587188250@qq.com>
2021-07-11 19:08:45 +0800

    比赛详情页修改

--- src/pages/Competition/Detail/index.tsx
+++ src/pages/Competition/Detail/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
-import { Card, Typography, Divider, Tag, Steps,} from 'antd';
+import { Card, Typography, Divider, Tag, Steps, Button, Space} from 'antd';
 const { Title, Paragraph, Text, Link } = Typography;
 const { Step } = Steps;
 
-const tagsData = ['校科协', '计软网安院科协', ];
+const tagsData = ['校科协', '计软网安院科协',];
+const StepNow = 3
+const StepData = [
+  ['报名时间', '1919.8.10'],
+  ['比赛时间', '2019.9.10'],
+  ['比赛结束'],
+  ['123'],
+  ['12345']];
 class Units extends React.Component {
   render() {
     return (
@@ -19,6 +26,19 @@ class Units extends React.Component {
     );
   }
 }
+class StepTag extends React.Component {
+  render() {
+    return (
+      <>
+        <Steps current={StepNow}>
+          {StepData.map(data => (
+            <Step title={data[0]} description={data[1]} />
+          ))}
+        </Steps>
+      </>
+    );
+  }
+}
 export default (): React.ReactNode => {
   return (
     <PageContainer>
@@ -26,12 +46,13 @@ export default (): React.ReactNode => {
         <Title>蓝桥杯程序设计大赛</Title>
         <Units />
         <Card>
-          <Steps current={1}>
-            <Step title="报名时间" description="2021.7.1-2021.7.5" />
-            <Step title="比赛时间" description="2021.7.10
-            11.45.14-19.19.00" />
-            <Step title="比赛结束" description="" />
-          </Steps>
+          <StepTag />
+        </Card>
+        <Card>
+          <Space>
+            <Button type="primary" href="/welcome" target="_blank" >比赛报名</Button>
+            <Button type="primary" href="/welcome" target="_top" >我的状态</Button>
+          </Space>
         </Card>
         <Card>
           <Title level={2}>比赛简介</Title>
