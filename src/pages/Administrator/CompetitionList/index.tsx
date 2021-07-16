eb3aaa9e5f50062e3f38baf51f6b5452cf627fcc
picpo <2587188250@qq.com>
2021-07-16 13:22:03 +0800

    比赛列表页完成

--- src/pages/Administrator/CompetitionList/index.tsx
+++ src/pages/Administrator/CompetitionList/index.tsx
@@ -1,28 +1,53 @@
 import React from 'react';
-import { Typography, Row, Col, Card, Button } from 'antd';
+import { Typography, Row, Col, Card, Button,Radio } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 const { Title, Paragraph, Text, Link } = Typography;
+
+const gridStyle = {
+  width: '400px',
+  height: '200px',
+};
+const NewStyle = {
+  height: '100%',
+  textAlign: 'center',
+};
+class ComCard extends React.Component {
+  render() {
+    return (
+      <>
+        <Card.Grid style={gridStyle}>
+          <Typography>
+            <Title level={3}>
+              xx比赛
+            </Title>
+                <Button>修改</Button>
+                <Button>状态</Button>
+                <Button>评委</Button>
+                <Button>结束</Button>
+          </Typography>
+        </Card.Grid>
+      </>
+    );
+  }
+}
 function CompetitionList() {
   return (
     <Typography>
       <Title>比赛列表</Title>
-      <Row gutter={[16, 24]}>
-        <Col span={8}>
-          <Card hoverable={true}>
-          </Card>
-        </Col>
-        <Col span={8}>
-          <Card hoverable={true}>
-          </Card>
-        </Col>
-        <Col span={8}>
-          <Card hoverable={true}>
-            <Button type="dashed" block={true} icon={<PlusOutlined />}>
+      <Card>
+        <ComCard />
+        <ComCard />
+        <ComCard />
+        <ComCard />
+        <ComCard />
+        <Card.Grid style={gridStyle}>
+          <a href="/admin/create-competition">
+            <Button type="dashed" block icon={<PlusOutlined />} style={NewStyle}>
               新建比赛
             </Button>
-          </Card>
-        </Col>
-      </Row>
+          </a>
+        </Card.Grid>
+      </Card>
     </Typography>
   );
 }
