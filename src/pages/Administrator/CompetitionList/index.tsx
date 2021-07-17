74033d20ab6eb843e0351633e1887f5e300ee435
picpo <2587188250@qq.com>
2021-07-17 23:43:49 +0800

    修改比赛列表UI，添加响应式

--- src/pages/Administrator/CompetitionList/index.tsx
+++ src/pages/Administrator/CompetitionList/index.tsx
@@ -1,31 +1,33 @@
 import React from 'react';
-import { Typography, Row, Col, Card, Button, Radio } from 'antd';
+import { Typography, Row, Col, Card, Button, Radio, Space } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 const { Title, Paragraph, Text, Link } = Typography;
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
-const gridStyle = {
-  width: '400px',
-  height: '200px',
+const CardStyle = {
+  height:'150px',
 };
 const NewStyle = {
-  height: '100%',
+  height:'100px',
   textAlign: 'center',
 };
-class ComCard extends React.Component {
+class ComCol extends React.Component {
   render() {
     return (
       <>
-        <Card.Grid style={gridStyle}>
-          <Typography>
+        <Col xs={24} md={12} xl={8}>
+          <Card style={CardStyle} hoverable
+            actions={[
+              <a href="">修改</a>,
+              <a href="/admin/competition-list/competition-state">状态</a>,
+              <a href="/admin/competition-list/judge-management">评委</a>,
+              <a href="">结束</a>,
+            ]}
+          >
             <Title level={3}>
               xx比赛
             </Title>
-            <Button href="">修改</Button>
-            <Button href="/admin/competition-list/competition-state">状态</Button>
-            <Button href="/admin/competition-list/judge-management">评委</Button>
-            <Button href="">结束</Button>
-          </Typography>
-        </Card.Grid>
+          </Card>
+        </Col>
       </>
     );
   }
@@ -34,18 +36,25 @@ function CompetitionList() {
   return (
     <PageHeaderWrapper title={<Title level={2}>比赛列表</Title>}>
       <Card>
-        <ComCard />
-        <ComCard />
-        <ComCard />
-        <ComCard />
-        <ComCard />
-        <Card.Grid style={gridStyle}>
-          <a href="/admin/create-competition">
-            <Button type="dashed" block icon={<PlusOutlined />} style={NewStyle}>
-              新建比赛
-            </Button>
-          </a>
-        </Card.Grid>
+        <Row gutter={[36, 18]}>
+          <ComCol />
+          <ComCol />
+          <ComCol />
+          <ComCol />
+          <ComCol />
+          <ComCol />
+          <ComCol />
+          <ComCol />
+          <Col xs={24} md={12} xl={8}>
+            <Card style={CardStyle} hoverable>
+              <a href="/admin/create-competition">
+                <Button type="dashed" block icon={<PlusOutlined />} style={NewStyle}>
+                  新建比赛
+                </Button>
+              </a>
+            </Card>
+          </Col>
+        </Row>
       </Card>
     </PageHeaderWrapper>
   );
