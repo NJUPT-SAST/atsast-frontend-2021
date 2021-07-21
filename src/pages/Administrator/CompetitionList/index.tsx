ae7ace757b089f3b68b6e605913b51031b5bb76e
picpo <2587188250@qq.com>
2021-07-21 17:04:28 +0800

    调了几个模拟API，可以作为参考

--- src/pages/Administrator/CompetitionList/index.tsx
+++ src/pages/Administrator/CompetitionList/index.tsx
@@ -1,32 +1,62 @@
 import React from 'react';
-import { Typography, Row, Col, Card, Button, Radio, Breadcrumb, PageHeader} from 'antd';
+import { Typography, Row, Col, Card, Button, Radio, Breadcrumb, PageHeader } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
+import request from 'umi-request';
+import reqwest from 'reqwest';
 const { Title, Paragraph, Text, Link } = Typography;
 const CardStyle = {
-  height:'150px',
+  height: '150px',
 };
 const NewStyle = {
-  height:'100px',
+  height: '100px',
   textAlign: 'center',
 };
+
 class ComCol extends React.Component {
+  state = {
+    list: [
+      {
+        contestId: "",
+        contestName: "",
+        contestOrganizer: "",
+        contestTime: "",
+        contestTimeEnd: "",
+        contestType: "",
+        currStu: "",
+      }
+    ]
+  }
+  componentDidMount() {
+    fetch('https://yapi.sast.fun/mock/13/user/contestlist/', {
+      method: 'get',
+      // mode: 'cors',
+    })
+      .then(res => res.json())
+      .then(json => {
+        this.setState({ list: json });
+        // console.log(this.state.list);
+      })
+  }
   render() {
+
     return (
       <>
-        <Col xs={24} md={12} xl={8}>
-          <Card style={CardStyle} hoverable
-            actions={[
-              <a href="">修改</a>,
-              <a href="/admin/competition-list/competition-state">状态</a>,
-              <a href="/admin/competition-list/judge-management">评委</a>,
-              <a href="">结束</a>,
-            ]}
-          >
-            <Title level={3}>
-              xx比赛
-            </Title>
-          </Card>
-        </Col>
+        {this.state.list.map(data => (
+          <Col xs={24} md={12} xl={8}>
+            <Card style={CardStyle} hoverable
+              actions={[
+                <a href="">修改</a>,
+                <a href="/admin/competition-list/competition-state">状态</a>,
+                <a href="/admin/competition-list/judge-management">评委</a>,
+                <a href="">结束</a>,
+              ]}
+            >
+              <Title level={3}>
+                {data.contestName}
+              </Title>
+            </Card>
+          </Col>
+        ))}
       </>
     );
   }
@@ -49,13 +79,6 @@ function CompetitionList() {
       />
       <Card>
         <Row gutter={[36, 18]}>
-          <ComCol />
-          <ComCol />
-          <ComCol />
-          <ComCol />
-          <ComCol />
-          <ComCol />
-          <ComCol />
           <ComCol />
           <Col xs={24} md={12} xl={8}>
             <Card style={CardStyle} hoverable>
